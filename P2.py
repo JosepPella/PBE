c8f3656efea6c2d8e0ad4696b023b227dfefd04d
JosepPella <146730156+JosepPella@users.noreply.github.com>
2023-10-18 08:59:51 +0200

    Update P2.py

--- P2.py
+++ P2.py
@@ -1,7 +1,6 @@
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import GLib, Gtk
-from gi.repository import Gdk
+from gi.repository import GLib, Gtk, Gdk
 import threading
 from P1 import Rfid
 
